54f6cf95a7c33bb44b9df0e2d23071ba15da57f1
Amilcar Aponte <apontejaj@gmail.com>
2021-08-22 07:00:29 +0100

    Initial commit

---
+++ src/test/java/com/apontejaj/accela/AccelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apontejaj.accela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
